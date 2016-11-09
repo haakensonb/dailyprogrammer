cca58a2ed555525c574f742c07c1844adb850db4
haakensonb <untamedhawk44@gmail.com>
2016-11-09 15:26:37 -0500

    Read input string and turn into array

--- challenge_291/challenge_291.js
+++ challenge_291/challenge_291.js
@@ -2,3 +2,14 @@
 [2016-11-07] Challenge #291 [Easy] Goldilocks' Bear Necessities
 */
 
+/*
+Read input file and turn string into more readable array i.e.
+	[
+		[weight, temp],
+		[weight, temp]...
+	]
+*/
+var fs = require('fs');
+var array = fs.readFileSync('input.txt').toString().replace(/\r/g, "").split('\n');
+array = array.map(x => x.split(' '));
+console.log(array);
