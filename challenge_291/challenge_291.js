aacec1f0e6b19b819c36190938f359f9e7e0410a
haakensonb <untamedhawk44@gmail.com>
2016-11-09 15:35:32 -0500

    Store goldilocks and remove from array

--- challenge_291/challenge_291.js
+++ challenge_291/challenge_291.js
@@ -12,4 +12,12 @@ Read input file and turn string into more readable array i.e.
 var fs = require('fs');
 var array = fs.readFileSync('input.txt').toString().replace(/\r/g, "").split('\n');
 array = array.map(x => x.split(' '));
+// console.log(array);
+
+const goldilocksWeight = array[0][0],
+	goldilocksTemp   = array[0][1];
+
+// console.log(goldilocksWeight, goldilocksTemp);
+// remove goldilocks from the array now that we already have her stored
+array.splice(0,1);
 console.log(array);
