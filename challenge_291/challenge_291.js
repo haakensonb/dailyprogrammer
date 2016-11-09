6263102c058d9029fb540f2b85dc9fe43276572c
haakensonb <untamedhawk44@gmail.com>
2016-11-09 16:50:36 -0500

    Code cleaned

--- challenge_291/challenge_291.js
+++ challenge_291/challenge_291.js
@@ -12,21 +12,23 @@ Read input file and turn string into more readable array i.e.
 var fs = require('fs');
 var array = fs.readFileSync('input.txt').toString().replace(/\r/g, "").split('\n');
 array = array.map(x => x.split(' '));
+
+//Push strings into new array as numbers
 var arrayNum = [];
 array.forEach(function(item){
 	arrayNum.push(item.map(Number));
 });
 
+//store goldilocks weigth and temperature
 var 	goldilocksWeight = arrayNum[0][0],
 		goldilocksTemp   = arrayNum[0][1];
 
 // remove goldilocks from the array now that we already have her stored
 arrayNum.splice(0,1);
 
-var results = [];
-
 arrayNum.forEach(function (item, index) {
 	if (goldilocksWeight <= item[0] && goldilocksTemp >= item[1]){
+		//add 1 to account for removing goldilocks
 		console.log(index + 1);
 	}
 });
