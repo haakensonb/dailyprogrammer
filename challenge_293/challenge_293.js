dccddbe98a39340c480114f7933b18feafb77296
haakensonb <untamedhawk44@gmail.com>
2016-11-21 16:55:26 -0500

    finished

--- challenge_293/challenge_293.js
+++ challenge_293/challenge_293.js
@@ -8,5 +8,38 @@ white
 `;
 
 input = input.trim().split('\n');
+// each input has an array of acceptable follow up inputs
+var rules = {
+    white: ['red', 'orange', 'green', 'purple'],
+    red: ['green'],
+    black: ['red', 'black', 'purple'],
+    orange: ['red', 'black'],
+    green: ['orange', 'white'],
+    purple: ['red', 'black']
+};
 
-console.log(input);
+function defuse () {
+    for (let i = 0; i < input.length; i++){
+        //if it passed the second to last input without blowing up, it is defused
+        if (i === input.length - 1) {
+            return 'Bomb defused';
+        } else if (check(input[i], input[i + 1]) === false){
+            return 'boom'
+        }
+    }
+}
+
+
+function check (curr, next) {
+    return myIncludes(rules[curr], next);
+}
+
+function myIncludes (rule, next) {
+    if (rule.indexOf(next) === -1) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+console.log(defuse());
